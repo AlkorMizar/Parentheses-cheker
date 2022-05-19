59f630e54d528ed4a062db1d223a44358cf8260d
AlkorMizar <anisa15012002@gmail.com>
2022-05-20 00:58:19 +0300

    changed path to conf

--- conf.go
+++ conf.go
@@ -3,6 +3,8 @@ package conf
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
+	"runtime"
 )
 
 type Config struct {
@@ -12,8 +14,14 @@ type Config struct {
 	Route   string `json:"route"`
 }
 
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath   = filepath.Dir(b)
+	confFile   = filepath.Join(basepath, "conf.json")
+)
+
 func NewConf() (*Config, error) {
-	f, err := os.Open("config.json")
+	f, err := os.Open(confFile)
 	if err != nil {
 		return nil, err
 	}
